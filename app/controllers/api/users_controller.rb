54964855df9506f6a8ef347165ecb0b7afa24be5
Brittiny Filbert <britfilbert@icloud.com>
2023-10-04 11:40:45 -0700

    add Api::UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,21 @@
 class Api::UsersController < ApplicationController
+  wrap_parameters include: User.attribute_names + ['password'] # ask about this
+
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render json: {user: @user}
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+    # render json: user_params
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:phone_number, :username, :password)
   end
 end
