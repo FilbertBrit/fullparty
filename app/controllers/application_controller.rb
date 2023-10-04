caafda6b6372e49c11dd705a29897ac630c57004
Brittiny Filbert <britfilbert@icloud.com>
2023-10-04 16:50:50 -0700

    backend complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
-    # protect_from_forgery with: :exception # csrf
+
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken,
+    with: :invalid_authenticity_token
+
+    protect_from_forgery with: :exception # csrf
+
     before_action :attach_authenticity_token, :snake_case_params
 
     def test
@@ -32,7 +38,7 @@ class ApplicationController < ActionController::API
     def logout!
         # reset the `current_user`'s session cookie, if one exists
         # clear out token from `session` cookie
-        current_user.reset_session_token! # remove token from db
+        current_user.reset_session_token! if current_user# remove token from db
         session[:session_token] = nil # remove token from browser cookies
         @current_user = nil # so that subsequent calls to `current_user` return nil
     end
@@ -69,4 +75,20 @@ class ApplicationController < ActionController::API
     params.deep_transform_keys!(&:underscore)
     end
       
+    def invalid_authenticity_token
+        render json: { message: 'Invalid authenticity token' }, 
+            status: :unprocessable_entity
+    end
+
+    def unhandled_error(error)
+        if request.accepts.first.html?
+            raise error
+        else
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error', status: :internal_server_error
+            
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+        end
+    end
 end
