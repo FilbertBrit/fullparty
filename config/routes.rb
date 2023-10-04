1991adfdcabed6e38fe25e6a376c7972d667ff2f
Brittiny Filbert <britfilbert@icloud.com>
2023-10-04 11:09:12 -0700

    add Api::sessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post 'api/test', to: 'application#test'
+  # post 'api/test', to: 'application#test'
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
-    resource :events, only: [:show, :create, :destroy, :index, :update, :index]
-    resources :rsvps, only: [:create, :update]
-    resources :comments, only: [:create, :update, :destroy]
+    # resource :events, only: [:show, :create, :destroy, :index, :update, :index]
+    # resources :rsvps, only: [:create, :update]
+    # resources :comments, only: [:create, :update, :destroy]
   end
 end
