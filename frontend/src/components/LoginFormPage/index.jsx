86552e949fa803d6d37cc037b8f9292147c26327
Brittiny Filbert <britfilbert@icloud.com>
2023-10-05 15:22:21 -0700

    create LoginFromPage component

---
+++ frontend/src/components/LoginFormPage/index.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ phoneNumber, password }))
+      .catch(async (res) => {
+        let data;
+        try {
+          // .clone() essentially allows you to read the response body twice
+          data = await res.clone().json();
+        } catch {
+          data = await res.text(); // Will hit this case if the server is down
+        }
+        if (data?.errors) setErrors(data.errors);
+        else if (data) setErrors([data]);
+        else setErrors([res.statusText]);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='loginForm'>
+      <ul>
+        {errors.map(error => <li key={error}>{error}</li>)}
+      </ul>
+      <label>
+        Phone Number
+        <input
+          type="text"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          required
+          name='phoneNumber'
+        />
+      </label>
+      <br/>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          name='password'
+        />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+
+export default LoginFormPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,25 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store';
 import csrfFetch, { restoreCSRF } from './store/csrf';
+import * as sessionActions from './store/session';
 
-const store = configureStore();
 
 //development purposes
+const store = configureStore();
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
   window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 const renderApplication = () => {
@@ -29,20 +41,3 @@ if (sessionStorage.getItem("X-CSRF-Token") === null) {
 } else {
   renderApplication();
 }
-
-function Root() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  );
-}
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Root/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
