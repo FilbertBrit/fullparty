b26a30d6f205dd720a771370f7b59a0f659f4eb1
Brittiny Filbert <britfilbert@icloud.com>
2023-10-18 11:33:13 -0700

    rsvp backend done

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,6 +2,20 @@ class Api::EventsController < ApplicationController
   wrap_parameters include: Event.attribute_names + ['dateTime', 'authorId']
   before_action :set_event, only: [:show, :update, :destroy]
 
+  def index
+    # debugger
+    @user = current_user
+    
+    # all events create by user
+    @events = Event.all.where(author_id: @user.id)
+    # @events = Event.all
+    render :index
+  end
+  
+  def show
+    render :show
+  end
+
   def create
     # debugger
     @event = Event.new(event_params)
@@ -12,19 +26,13 @@ class Api::EventsController < ApplicationController
     end
     # render json: event_params
   end
-
-  def index
-    # debugger
-    @user = current_user
-
-    # all events create by user
-    @events = Event.all.where(author_id: @user.id)
-    # @events = Event.all
-    render :index
-  end
-
-  def show
-    render :show
+  
+  def update
+    if @event.update(event_params)
+      render :show
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
   end
 
   def destroy
@@ -34,14 +42,6 @@ class Api::EventsController < ApplicationController
     # debugger
     render :index
     # render json: {events: @events}
-  end
-
-  def update
-    if @event.update(event_params)
-      render :show
-    else
-      render json: @event.errors.full_messages, status: 422
-    end
   end
 
   private
---
+++ app/controllers/api/rsvps_controller.rb
@@ -0,0 +1,40 @@
+class Api::RsvpsController < ApplicationController
+  wrap_parameters include: Rsvp.attribute_names + ['userId', 'eventId']
+  before_action :set_rsvp, only: [:show, :update]
+
+  def show 
+    render :show
+  end
+
+  def create
+    @rsvp = Rsvp.new(rsvp_params)
+    if @rsvp.save
+      # render :show
+      # render json: rsvp_params
+    else
+      render json: {errors: @rsvp.errors.full_messages }, status: :unprocessable_entity
+    end
+    # render json: rsvp_params
+  end
+
+  def update
+    if @rsvp.update(rsvp_params)
+      # render :show
+    else
+      render json: @rsvp.errors.full_messages, status: 422
+    end
+  end
+
+  private 
+
+  def set_rsvp
+    @rsvp = Rsvp.find(params[:id])
+
+    rescue
+      render json: ['RSVP not found'], status: :not_found
+  end
+
+  def rsvp_params
+    params.require(:rsvp).permit(:status, :user_id, :event_id)
+  end
+end
