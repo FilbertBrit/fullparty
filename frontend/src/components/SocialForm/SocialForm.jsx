5caec12f1cd56cb9530ed2a6e0c448c901d55403
Brittiny Filbert <britfilbert@icloud.com>
2023-11-25 23:54:06 -0500

    add modal social form styling

--- frontend/src/components/SocialForm/SocialForm.jsx
+++ frontend/src/components/SocialForm/SocialForm.jsx
@@ -1,12 +1,23 @@
-
+import "./SocialForm.css"
 
 export const SocialForm = ({social}) => {
     console.log(social)
     console.log('here')
 
+    const handleSubmit = () => {
+
+    }
+
     return(
-        <div>
-            <h3>Socials</h3>
+        <div className="social-compnt">
+            <form id="social-form">
+                <h5 id="social-header">{social}</h5>
+                <div>
+                    <input type="text" placeholder="Your username" id="social-input" />
+                </div>
+                <button onClick={handleSubmit} className="social-confirm-btn"> CONFIRM </button>
+            </form>
+            {/* <h3>Socials</h3> */}
         </div>
     )
 }
