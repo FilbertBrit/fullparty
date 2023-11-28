93a3a0de1700bca882b44a5a95ed057a35e77509
Brittiny Filbert <britfilbert@icloud.com>
2023-11-27 22:01:22 -0500

    update filters to reflect event dates

--- frontend/src/components/Events/EventIndex/index.jsx
+++ frontend/src/components/Events/EventIndex/index.jsx
@@ -12,9 +12,9 @@ export const EventIndex = ({filter}) => {
     const sessionUser = useSelector(state => state.session.user);
     const eventsObj = useSelector(getEvents);
     const events = eventsObj ? Object.values(eventsObj) : [];
-    const today = new Date('2023-12-05T21:00:00');
-    console.log(today, new Date(events[0]?.dateTime))
-    console.log(today < new Date(events[0]?.dateTime))
+    const today = new Date();
+    // console.log(today, new Date(events[0]?.dateTime))
+    // console.log(today < new Date(events[0]?.dateTime) )
     let filteredEvents = []
 
     useEffect( () => {
@@ -23,15 +23,15 @@ export const EventIndex = ({filter}) => {
 
     //still need to take into account dates, old events vs current events
     if(filter === "Upcoming"){
-        filteredEvents = events.filter(event => event.userRsvp !== "null");//=== "I'm Going" || event.userRsvp === 'Maybe');
+        filteredEvents = events.filter(event => (today < new Date(event.dateTime)) && (event.userRsvp !== "null"));//=== "I'm Going" || event.userRsvp === 'Maybe');
     }else if(filter === "Hosting"){
-        filteredEvents = events.filter(event => event.authorId === sessionUser.id);
+        filteredEvents = events.filter(event => (today < new Date(event.dateTime)) && (event.authorId === sessionUser.id));
     }else if(filter === "Open Invite"){
-        filteredEvents = events;
+        filteredEvents = events.filter(event => today < new Date(event.dateTime));
     }else if(filter === 'Attended'){
-
+        filteredEvents = events.filter(event => (today > new Date(event.dateTime)) && (event.userRsvp !== "null"));
     }else if(filter === 'All Past Events'){
-        filteredEvents = events.filter(event => (event.date) && ((event.authorId === sessionUser.id ) || (event.userRsvp !== 'null')));
+        filteredEvents = events.filter(event => (today > new Date(event.dateTime)) && ((event.authorId === sessionUser.id ) || (event.userRsvp !== 'null')));
     }
 
     
