86f53a6adc54aea4fd7c186ca1d3a4ff4c8f80b5
Brittiny Filbert <britfilbert@icloud.com>
2023-12-22 20:51:20 -0800

    create mutuals component

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -79,6 +79,9 @@ export function HomePage () {
                         <h1 id="welcome-header">Mutuals</h1>
                         <a href="" id="all-mutuals-link">SEE ALL</a>
                     </div>
+                    <div id="mutuals-preview">
+                        
+                    </div>
                 </div>
                 <div id='footer'>© 2023 FullParty™ | Terms & Privacy | Careers | Questions? DM us <AiOutlineInstagram/></div>
             </div>
