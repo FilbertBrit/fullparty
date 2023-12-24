eac8c0ebf224a0a5b322464d64e057eb41639fa6
Brittiny Filbert <britfilbert@icloud.com>
2023-12-23 19:32:16 -0800

    mutual prev visiable

--- frontend/src/components/HomePage/MutualsItem.jsx
+++ frontend/src/components/HomePage/MutualsItem.jsx
@@ -3,10 +3,26 @@
 export function MutualsItem ({ mutual}) {
 
 
-    console.log(mutual)
+    const handleClick = (e) => {
+        e.preventDefault();
+        // console.log('hi')
 
+    }
 
     return (
-        <div></div>
+        <div id="mutual-prev-item" onClick={handleClick}>
+            <div id="photo-container-mutual-prev">
+                <img src=""></img>
+            </div>
+            <div id="username-container-mutual-prev">
+                <h2>{mutual.name}</h2>
+            </div>
+            <div id="#-events-container">
+                <h2>#</h2>
+            </div>
+            <div id="last-shared-event">
+                <h2>event</h2>
+            </div>
+        </div>
     )
 }
