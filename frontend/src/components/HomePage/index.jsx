5493186bac69422368e56f8d78c7545762a0cd74
Brittiny Filbert <britfilbert@icloud.com>
2024-02-01 23:29:19 -0800

    update nav bar styling

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -85,19 +85,19 @@ export function HomePage () {
                     <button className="event-filter-btn-open" style={{
                         background: 
                             filter === "Open Invite" ? "linear-gradient(0deg,rgba(0,0,0,.3),rgba(0,0,0,.3)),linear-gradient(90deg,rgba(217,149,211,.6) 7.51%,rgba(100,98,239,.6) 28.85%,rgba(109,238,255,.6) 57.53%,rgba(190,254,246,.6) 68.5%,rgba(118,246,185,.6) 82.82%,rgba(191,255,224,.6) 97.32%),hsla(0,0%,100%,.15)"
-                             : "transparent",
+                             : "hsla(0,0%,100%,.025)",
                         }} onClick={handleClick} value="Open Invite">
                         💫 Open Invite
                     </button>
                     <button className="event-filter-btn" style={{
                         backgroundColor: 
-                            filter === "Upcoming" ? "hsla(0,0%,100%,.15)" : "transparent",
+                            filter === "Upcoming" ? "hsla(0,0%,100%,.15)" : null,
                         }} onClick={handleClick} value="Upcoming">
                         Upcoming
                     </button>
                     <button className="event-filter-btn" style={{
                         backgroundColor: 
-                            filter === "Hosting" ? "hsla(0,0%,100%,.15)" : "transparent",
+                            filter === "Hosting" ? "hsla(0,0%,100%,.15)" : null,
                         }} onClick={handleClick} value="Hosting">
                         Hosting
                     </button>
@@ -106,13 +106,13 @@ export function HomePage () {
                     </button> */}
                     <button className="event-filter-btn" style={{
                         backgroundColor: 
-                            filter === "Attended" ? "hsla(0,0%,100%,.15)" : "transparent",
+                            filter === "Attended" ? "hsla(0,0%,100%,.15)" : null,
                         }} onClick={handleClick} value="Attended">
                         Attended
                     </button>
-                    <button className="event-filter-btn" style={{
+                    <button className="event-filter-btn" id="event-filters" style={{
                         backgroundColor: 
-                            filter === "All Past Events" ? "hsla(0,0%,100%,.15)" : "transparent",
+                            filter === "All Past Events" ? "hsla(0,0%,100%,.15)" : null,
                         }} onClick={handleClick} value="All Past Events">
                         All Past Events
                     </button>
