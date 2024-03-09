e8982a8bda617c0b79d4ccb751fad8b59966b4b1
Brittiny Filbert <britfilbert@icloud.com>
2024-03-09 13:44:31 -0800

    complete css for invite form checkbox

--- frontend/src/components/InviteModal/MutualInvitee.jsx
+++ frontend/src/components/InviteModal/MutualInvitee.jsx
@@ -3,11 +3,21 @@ import { useState } from "react"
 export const MutualInvitee = ({ mutual }) => {
 
     const [clicked, setClicked] = useState(false);
-    console.log(mutual, clicked)
+    const [clickedStyle, setClickedStyle] = useState('invitee-checkbox-unchecked');
+    console.log(clickedStyle, clicked)
 
+    const handleClick = () => {
+        if(clicked === false){
+            setClicked(true);
+            setClickedStyle('invitee-checkbox-checked');
+        }else{
+            setClicked(false);
+            setClickedStyle('invitee-checkbox-unchecked');
+        }
+    }
 
     return (
-        <div className="MutualInvitee" onClick={ () => clicked === false ? setClicked(true) : setClicked(false)}>
+        <div className="MutualInvitee" onClick={ () => handleClick()}>
             <div id="invitee-user-photo">
                 <div className="initials" id='initial-mutual-invitee'>
                     {mutual.name.slice(0,1)}
@@ -17,7 +27,7 @@ export const MutualInvitee = ({ mutual }) => {
                 <h2 id='invitee-name'>{mutual.name}</h2>
                 <p id='invitee-event'>🕔 {mutual.recentEvent}</p>
             </div>
-            <div id="invitee-checkbox">
+            <div className={clickedStyle} id='invitee-checkbox'>
                 {clicked ? '✓' : null}
             </div>
         </div>
