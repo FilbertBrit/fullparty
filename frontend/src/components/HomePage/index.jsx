e28df73514a713cf1fcf7e02670185afb344f333
Brittiny Filbert <britfilbert@icloud.com>
2024-04-19 21:53:19 -0700

    update logic for all past events filter

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -50,7 +50,8 @@ export function HomePage () {
             filteredEvents = events.filter(event => (event.userRsvp[0] === "I'm Going") && (today > new Date(event.dateTime) && event.dateTime !== null));
             break;
         case 'All Past Events':
-            filteredEvents = events.filter(event => ((event.dateTime !== null) && today > new Date(event.dateTime)) && ((event.authorId === sessionUser.id ) || (event.userRsvp !== null)));
+            filteredEvents = events.filter(event => (event.userRsvp[0] !== null && today > new Date(event.dateTime)));
+            // filteredEvents = events.filter(event => ((event.dateTime !== null) && today > new Date(event.dateTime)) && ((event.authorId === sessionUser.id ) || (event.userRsvp !== null)));
             break;
         case 'Invites':
             filteredEvents = events.filter(event => (event.userRsvp[0] === 'invited' && (today < new Date(event.dateTime) || event.dateTime === null)));
