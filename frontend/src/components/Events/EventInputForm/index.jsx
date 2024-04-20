ed8688b279fbc622b2c4637a1fd183aa77e55cec
Brittiny Filbert <britfilbert@icloud.com>
2024-04-19 23:31:09 -0700

    add cancel btn to event edit form

--- frontend/src/components/Events/EventInputForm/index.jsx
+++ frontend/src/components/Events/EventInputForm/index.jsx
@@ -306,6 +306,13 @@ export function EventInputForm () {
                             <h2>☑️</h2>
                             <h2 id='author-nav-sidebar-text' >DONE</h2>
                         </div>
+                        <div className="module-invite" id='author-nav-sidebar-divider'>
+                            <div className="divider-input"></div>
+                        </div>
+                        <div className="module-invite" id='author-nav-sidebar-item'>
+                            <h2>🔴</h2>
+                            <h2 id='author-nav-sidebar-text' >CANCEL</h2>
+                        </div>
                     </div>
                 ) : (
                     // <div>
