92ed0ca9d62279cb644ce8263cfb32b9f9f4af97
Brittiny Filbert <britfilbert@icloud.com>
2024-05-13 10:39:45 -0700

    notifications display sender + event

--- frontend/src/components/Navigation/NotificationItem.jsx
+++ frontend/src/components/Navigation/NotificationItem.jsx
@@ -2,21 +2,21 @@ import './Navigation.css';
 
 export function NotificationItem ({ notification }) {
     console.log(notification)
-    // const eventShowPage = '/events/' + notification.eventId;
-    // const userProfile = '/users/' + notification.senderId;
+    const eventShowPage = '/events/' + notification.eventId;
+    const userProfile = '/users/' + notification.senderId;
 
     return(
-        // <a href={eventShowPage}>
-        //     <div>
-        //         <div className='notification-prof-photo'>
+        <a href={eventShowPage}>
+            <div>
+                <div className='notification-prof-photo'>
 
-        //         </div>
-        //         <div className='notification-info'>
-        //             <div><a href={userProfile}>{notification.sender}</a> invited you to an event</div>
-        //             {/* <p></p> */}
-        //         </div>
-        //     </div>
-        // </a>
-        <></>
+                </div>
+                <div className='notification-info'>
+                    <div><a href={userProfile}>{notification.sender}</a> invited you to an event</div>
+                    {/* <p></p> */}
+                </div>
+            </div>
+        </a>
+        // <></>
     )
 }
