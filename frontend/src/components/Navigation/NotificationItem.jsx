83980512be52d3f5bd50e75dcb281197a5b145b6
Brittiny Filbert <britfilbert@icloud.com>
2024-05-14 20:07:49 -0700

    add css updates on notificationItem

--- frontend/src/components/Navigation/NotificationItem.jsx
+++ frontend/src/components/Navigation/NotificationItem.jsx
@@ -1,4 +1,6 @@
 import './Navigation.css';
+import './NotificationItem.css';
+
 
 export function NotificationItem ({ notification }) {
     console.log(notification)
@@ -6,14 +8,23 @@ export function NotificationItem ({ notification }) {
     const userProfile = '/users/' + notification.senderId;
 
     return(
-        <a href={eventShowPage}>
-            <div>
+        <a href={eventShowPage} className='notificationItem'>
+            <div className='notificationItemContainer'>
                 <div className='notification-prof-photo'>
 
                 </div>
                 <div className='notification-info'>
-                    <div><a href={userProfile}>{notification.sender}</a> invited you to an event</div>
-                    {/* <p></p> */}
+                    <div>
+                        <a href={userProfile} className='notification-item-user-link'>{notification.sender} </a>
+                         invited you to an event
+                    </div>
+                    <div className='notification-item-event-prev'>
+                        event prev
+                    </div>
+                    <div className='notification-item-msg'>
+                        <p> msg </p>
+                    </div>
+                    <p className='notication-item-date'> {notification.date} </p>
                 </div>
             </div>
         </a>
